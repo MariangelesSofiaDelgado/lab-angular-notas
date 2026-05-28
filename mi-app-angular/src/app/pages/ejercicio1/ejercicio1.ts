56bb0b231a6880884a00742dcb5e21cfff6bf905
MariangelesSofiaDelgado <estudiosengeneralmp@gmail.com>
2026-05-28 16:44:00 +0000

    Cambios del examen

--- mi-app-angular/src/app/pages/ejercicio1/ejercicio1.ts
+++ mi-app-angular/src/app/pages/ejercicio1/ejercicio1.ts
@@ -1,9 +1,70 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-ejercicio1',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './ejercicio1.html',
   styleUrl: './ejercicio1.css',
 })
-export class Ejercicio1 {}
+export class Ejercicio1 {
+  codigo: number = 0;
+  nombres: string = '';
+  n1: number = 0;
+  n2: number = 0;
+  n3: number = 0;
+  n4: number = 0;
+  suma: number = 0;
+  promedio: number = 0;
+  estado: string = '';
+  observacion: string = '';
+  mensaje = '';
+
+  procesar(): void {
+
+      if (!this.regularnota()) {
+        this.mensaje = 'Todas las notas deben estar entre 0 y 20.';
+        this.limpiar(); 
+        return;
+      }
+      this.suma = this.n1 + this.n2 + this.n3 + this.n4;
+      this.promedio = this.suma / 4;
+      this.estado = this.promedio >= 13 ? 'Aprobado' : 'Desaprobado';
+
+      if (this.promedio >= 17) {
+        this.observacion = 'EXCELENTE';
+      } else if (this.promedio >= 13) {
+        this.observacion = 'REGULAR';
+      } else {
+        this.observacion = 'EN RIESGO';
+      }
+    }
+
+    regularnota(): boolean {
+      return (
+        this.n1 >= 0 && this.n1 <= 20 &&
+        this.n2 >= 0 && this.n2 <= 20 &&
+        this.n3 >= 0 && this.n3 <= 20 &&
+        this.n4 >= 0 && this.n4 <= 20
+      );
+    }
+
+    limpiar(): void {
+      this.codigo = 0;
+      this.nombres = '';
+      this.n1 = 0;
+      this.n2 = 0;
+      this.n3 = 0;
+      this.n4 = 0;
+      this.suma = 0;
+      this.promedio = 0;
+      this.estado = '';
+      this.observacion = '';
+      this.mensaje = '';
+    }
+  }
+
+
+
--- mi-app-angular/src/app/pages/ejercicio2/ejercicio2.ts
+++ mi-app-angular/src/app/pages/ejercicio2/ejercicio2.ts
@@ -1,9 +1,63 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio2',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './ejercicio2.html',
   styleUrl: './ejercicio2.css',
 })
-export class Ejercicio2 {}
+export class Ejercicio2 {
+  nombre: string = '';
+  correo: string = '';
+  peso: number = 0;
+  altura: number = 0;
+  imc: number = 0;
+  estado: string = '';
+  emailValido: boolean = true;
+
+  procesar(): void {
+    if (this.altura <= 0) {
+      this.estado = 'La altura debe ser mayor que cero.';
+      return;
+    }
+
+    if (this.peso <= 0) {
+      this.estado = 'El peso debe ser mayor que cero.';
+      return;
+    }
+
+    if (this.nombre.trim() === '') {
+      this.estado = 'Por favor, ingrese su nombre antes de continuar.';
+      return;
+    }
+
+    this.validarEmail();
+    if (!this.emailValido) {
+      this.estado = 'Por favor, ingrese un correo electrónico válido antes de continuar.';
+      return;
+    }
+
+    this.imc = this.peso / (this.altura * this.altura);
+    if (this.imc < 18.5) {
+      this.estado = 'Bajo peso';
+    } else if (this.imc < 25) {
+      this.estado = 'Peso normal';
+    } else if (this.imc < 30) {
+      this.estado = 'Sobrepeso';
+    } else {
+      this.estado = 'Obesidad';
+    }
+  }
+
+
+validarEmail(): void {
+    if (this.correo.trim() === '') {
+      this.emailValido = false;
+      return;
+    }
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    this.emailValido = re.test(this.correo);
+  }
+}
