eec7fc9cdd0259757e45152eefd3cddd3923aa3d
MariangelesSofiaDelgado <estudiosengeneralmp@gmail.com>
2026-05-28 10:35:52 +0000

    nav examen

--- mi-app-angular/src/app/app.routes.ts
+++ mi-app-angular/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Productos } from './pages/productos/productos';
 import { Registro } from './pages/registro/registro';
+import { Ejercicio1 } from './pages/ejercicio1/ejercicio1';
+import { Ejercicio2 } from './pages/ejercicio2/ejercicio2';
 
 export const routes: Routes = [ 
     { 
@@ -10,4 +12,6 @@ export const routes: Routes = [
 { path: 'inicio', component: Inicio }, 
 { path: 'productos', component: Productos }, 
 { path: 'registro', component: Registro }, 
+{ path: 'ejercicio1', component: Ejercicio1},
+{ path: 'ejercicio2', component: Ejercicio2},
 { path: '**', redirectTo: 'inicio' } ];
